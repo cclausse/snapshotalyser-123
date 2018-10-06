fa240e81e6d3aaf39fc2e699f99117582f306e23
Carsten Claussen <cclausse@gmail.com>
2018-10-06 23:06:31 +0200

    Updated shotty.pyreadme.md

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,5 +4,6 @@ if __name__ == '__main__':
     session = boto3.Session(profile_name='shotty')
     ec2 = session.resource('ec2')
 
+    print('My EC2 Instances:')
     for i in ec2.instances.all():
         print(i)
